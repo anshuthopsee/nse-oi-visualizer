4fc99a5387bbced610c8b8d4838e35911e35dab3
Anshu Thopsee <anshuthopseecodes@gmail.com>
2024-06-15 15:35:06 +0100

    change app icon and favicon

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -2,10 +2,10 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getThemeMode, setThemeMode, type ThemeMode } from '../features/theme/themeSlice';
 import { setDrawerState } from '../features/drawer/drawerSlice';
 import { AppBar, Toolbar, IconButton, Typography } from '@mui/material';
-import SignalCellularAltIcon from '@mui/icons-material/SignalCellularAlt';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import MenuIcon from '@mui/icons-material/Menu';
+import trendIcon from '../assets/trend-icon.svg';
 import "@fontsource/exo/400.css";
 
 const Header = () => {
@@ -20,11 +20,14 @@ const Header = () => {
     <AppBar position='fixed' elevation={0} sx={{ backgroundColor: "background.paper", borderBottom: 1, borderBottomColor: "divider" }}>
       <Toolbar disableGutters>
         <div style={{ flexGrow: 1, display: "inline-flex", alignItems: "center" }}>
-          <SignalCellularAltIcon sx={{ color: "#c203fc", ml: 1, fontSize: { xs: "16px", sm: "18px" } }}/>
+          {/* <SignalCellularAltIcon sx={{ color: "#c203fc", ml: 1, fontSize: { xs: "16px", sm: "18px" } }}/> */}
+          <Typography color="inherit" component="div" sx={{ height: { xs: "30px", sm: "35px" }, width: { xs: "30px", sm: "35px" }, ml: "10px", mr: "5px" }}>
+            <img src={trendIcon} alt="logo" style={{ height: "100%", width: "100%" }}/>
+          </Typography>
           <Typography variant="h6" color="inherit" component="div" 
             sx={{ pr: 1, fontWeight: "bold", color: "text.primary", fontFamily: "Exo", fontSize: { xs: "16px", sm: "18px" } }}
           >
-            NSE-OI-VISUALIZER
+            nse-oi-visualizer
           </Typography>
         </div>
         <IconButton edge="start" color="inherit" aria-label="menu"
